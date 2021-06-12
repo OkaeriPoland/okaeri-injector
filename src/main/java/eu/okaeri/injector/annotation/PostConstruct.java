42319c70a15d11b35d035704c9ae27457d67bca0
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-06-12 22:35:54 +0200

    Add PostConstruct order, allow invoking methods/constructor, make all() immutable, add removeIf(filter), release 1.5.0

--- src/main/java/eu/okaeri/injector/annotation/PostConstruct.java
+++ src/main/java/eu/okaeri/injector/annotation/PostConstruct.java
@@ -8,4 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PostConstruct {
+    int order() default Integer.MAX_VALUE;
 }
