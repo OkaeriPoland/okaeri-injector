325210510853eb47041bf765722333564b76aa8a
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-07-10 10:22:26 +0200

    Replace explicit null checks with generated NonNull, release 1.5.1

--- src/main/java/eu/okaeri/injector/Injectable.java
+++ src/main/java/eu/okaeri/injector/Injectable.java
@@ -3,12 +3,13 @@ package eu.okaeri.injector;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Injectable<T> {
 
-    public static <T> Injectable<T> of(String name, T object, Class<T> type) {
+    public static <T> Injectable<T> of(@NonNull String name, @NonNull T object, @NonNull Class<T> type) {
         return new Injectable<>(name, object, type);
     }
 
