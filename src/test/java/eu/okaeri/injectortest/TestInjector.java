c4a21621b1577bf7e6cdd35041170312b5d255a2
Sandra <sandra@okaeri.cloud>
2021-09-21 08:09:26 +0200

    Make injector always provide the newest injectable, add registerExclusive, release 1.5.2

--- src/test/java/eu/okaeri/injectortest/TestInjector.java
+++ src/test/java/eu/okaeri/injectortest/TestInjector.java
@@ -1,11 +1,14 @@
 package eu.okaeri.injectortest;
 
+import eu.okaeri.injector.Injectable;
 import eu.okaeri.injector.Injector;
 import eu.okaeri.injector.OkaeriInjector;
 import eu.okaeri.injectortest.element.Api;
 import eu.okaeri.injectortest.element.Worker;
 import eu.okaeri.injectortest.element.Worker2;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -54,6 +57,38 @@ public final class TestInjector {
         Worker worker = injector.createInstance(Worker.class);
         assertEquals(api2, worker.getApi());
         assertEquals(3, worker.getCounter());
+    }
+
+    @Test
+    public void test_always_provide_last() {
+
+        String string1 = "some value";
+        String string2 = "new value";
+
+        Injector injector = OkaeriInjector.create()
+                .registerInjectable("test1", string1)
+                .registerInjectable("test1", string2);
+
+        Optional<? extends Injectable<String>> injectable = injector.getExact("test1", String.class);
+        assertEquals(true, injectable.isPresent());
+        assertEquals(string2, injectable.get().getObject());
+    }
+
+    @Test
+    public void test_exclusive_register() {
+
+        String string1 = "some value";
+        String string2 = "new value";
+
+        Injector injector = OkaeriInjector.create()
+                .registerInjectable("test1", string1)
+                .registerInjectable("test1", string2)
+                .registerExclusive("test1", string1);
+
+        Optional<? extends Injectable<String>> injectable = injector.getExact("test1", String.class);
+        assertEquals(true, injectable.isPresent());
+        assertEquals(string1, injectable.get().getObject());
+        assertEquals(1, injector.allOf(String.class).size());
     }
 }
 
